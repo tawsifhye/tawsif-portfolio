25c382780f82d1df40f50e2679ae6bbc47e61fe2
tawsifhye <tawsif.lucse@gmail.com>
2021-11-22 13:05:46 +0600

    Home Banner About components created

---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div>
+            <h1 className="text-2xl text-center">About Me</h1>
+        </div>
+    );
+};
+
+export default AboutMe;
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../ComponentGlobal.css'
+const Banner = () => {
+    return (
+        <>
+            <div className="banner-container text-5xl flex flex-col justify-center items-center">
+                <h1>I am Tawsif</h1>
+                <h1>A Web Developer</h1>
+            </div>
+        </>
+    );
+};
+
+export default Banner;
