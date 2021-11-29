77877399910ad285ffe4019882a1d2cb895ffdd0
tawsifhye <tawsif.lucse@gmail.com>
2021-11-29 13:42:33 +0600

    banner title nad image set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import AboutMe from './Components/AboutMe/AboutMe';
 import Home from './Components/Home/Home';
 
 function App() {
   return (
     <>
       <Home></Home>
-      <AboutMe></AboutMe>
+
     </>
   );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutMe from '../AboutMe/AboutMe';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <>
             <div>
                 <Banner></Banner>
+                <AboutMe></AboutMe>
             </div>
         </>
     );
