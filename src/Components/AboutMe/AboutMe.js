af6ad3be20cee0d38637dad86ef5be1dca5b8900
tawsifhye <tawsif.lucse@gmail.com>
2021-11-29 16:04:28 +0600

    About me added

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import bannar_image2 from "../../images/banner-image2.jpg"
 const AboutMe = () => {
     return (
-        <div>
-            <h1 className="text-2xl text-center">About Me</h1>
-        </div>
+        <>
+            <div id="about" className="container mt-28 grid grid-cols-2">
+                <div className="">
+                    <img className="mx-auto w-1/2 rounded-full ring-8 " src={bannar_image2} alt="" />
+                </div>
+                <div>
+                    <h1 className="text-5xl text-center text-green-800 font-bold">About Me</h1>
+                    <p className="mt-10 p-5 font-black">I'm a web developer with hands-on experience in JavaScript. I am passionate to build any kind of web development solution. I am an aspiring web developer, learning every aspect of web development. Alongside, I have achieved my CCNA certification. I have the experience to work as Technical Support Engineer. I am a quick learner, a team performer, and can stick to something until I achieve positive results.</p>
+                </div>
+                <div id="skills">
+
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../ComponentGlobal.css'
-import bannar_image1 from "../../images/banner-image1.png"
 import bannar_image2 from "../../images/banner-image2.jpg"
 const Banner = () => {
     return (
         <>
             <div className="grid grid-cols-2 banner-container">
                 <div className=" text-5xl flex flex-col justify-center align-middle  banner-title">
-                    <div className="mx-auto">
-                        <h1>I am Tawsif</h1>
+                    <div className="mx-auto font-semibold">
+                        <h1>Hi, I am Tawsif</h1>
                         <h1>A Web Developer</h1>
                     </div>
+
                 </div>
                 <div className="flex flex-col justify-center align-middle banner-image p-5">
                     <img className="w-3/5 rounded-full ring-8 " src={bannar_image2} alt="" />
