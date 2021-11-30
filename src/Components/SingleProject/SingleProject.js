651b69c1e988dff5176e2eb06d3898d3ede6c897
tawsifhye <tawsif.lucse@gmail.com>
2021-12-01 00:49:45 +0600

    slider speed changed

--- src/Components/SingleProject/SingleProject.js
+++ src/Components/SingleProject/SingleProject.js
@@ -9,7 +9,7 @@ const SingleProject = ({ project }) => {
         infinite: true,
         speed: 500,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
         slidesToShow: 1,
         slidesToScroll: 1
     };
