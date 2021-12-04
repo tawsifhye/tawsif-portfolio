e47bf863c3937df770dd6a76f6fb75d95e5159e5
tawsifhye <tawsif.lucse@gmail.com>
2021-12-04 22:01:43 +0600

    singleproject bullet point added and button padding changed

--- src/Components/SingleProject/SingleProject.js
+++ src/Components/SingleProject/SingleProject.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import '../ComponentGlobal.css'
 const SingleProject = ({ project }) => {
-    const { id, title, details, technologies, previews, website, githubClient, githubServer } = project;
+    const { id, title, details, technologies, previews, website, githubClient, githubServer, features } = project;
     const settings = {
         dots: false,
         infinite: true,
@@ -38,21 +38,25 @@ const SingleProject = ({ project }) => {
                                     <div className="mt-1 py-2">
                                         <h4 >Technology Used:</h4>
                                         <p>{technologies}</p>
+                                        <ul>
+                                            {features.map(feature => <li key={id} className="px-2"> <span className="px-1">➼</span> {feature}</li>)}
+                                        </ul>
+
                                     </div>
                                 </div>
                                 <a className="mr-2" href={website} rel="noreferrer" target="_blank">
-                                    <button className="mt-2 bg-blue-400 p-3 px-4 rounded-lg font-bold hvr-sweep-to-right"> <i className="fas fa-link text-xl"></i>
+                                    <button className="mt-2 bg-blue-400 p-3 px-2 rounded-lg font-bold hvr-sweep-to-right"> <i className="fas fa-link text-xl"></i>
                                         Website
                                     </button>
                                 </a>
                                 <a className="mr-2" href={githubClient} rel="noreferrer" target="_blank">
-                                    <button className="mt-2 bg-blue-400 p-3 px-4 rounded-lg font-bold hvr-sweep-to-right"> <i className="fab fa-github text-xl"></i>
+                                    <button className="mt-2 bg-blue-400 p-3 px-2 rounded-lg font-bold hvr-sweep-to-right"> <i className="fab fa-github text-xl"></i>
                                         Client Code
                                     </button>
                                 </a>
                                 {
                                     githubServer && <a className="mr-2" href={githubServer} rel="noreferrer" target="_blank">
-                                        <button className="mt-2 bg-blue-400 p-3 px-4 rounded-lg font-bold hvr-sweep-to-right"> <i className="fab fa-github text-xl"></i>
+                                        <button className="mt-2 bg-blue-400 p-3 px-2 rounded-lg font-bold hvr-sweep-to-right"> <i className="fab fa-github text-xl"></i>
                                             Server Code
                                         </button>
                                     </a>
