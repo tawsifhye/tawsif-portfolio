48f43cf30a5e1586dbbe6ca0613abbe3eaae8724
tawsifhye <tawsif.lucse@gmail.com>
2021-12-17 21:57:04 +0600

    react-reveal animation added

--- src/Components/FeaturedProjects/FeaturedProjects.js
+++ src/Components/FeaturedProjects/FeaturedProjects.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Fade } from 'react-reveal';
 import SingleProject from '../SingleProject/SingleProject';
 
 const FeaturedProjects = () => {
@@ -12,12 +13,13 @@ const FeaturedProjects = () => {
     return (
         <>
             <div id="projects" className="mt-28 mb-16">
-                <h1 className=" text-5xl text-center text-green-800 font-bold">Featured Projects</h1>
+                <Fade right>
+                    <h1 className=" text-5xl text-center text-green-800 font-bold">Featured Projects</h1>
+                </Fade>
                 <div className="container mt-10">
                     {
                         projects.map(project => <SingleProject key={project.id} project={project}></SingleProject>)
                     }
-
                 </div>
             </div>
         </>
