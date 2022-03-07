a23723d2f775c5e610ab5e3e936ecd76d1a222d0
unknown <tawsif.lucse@gmail.com>
2022-03-07 14:58:19 +0600

    About Me Updated

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -15,7 +15,7 @@ const AboutMe = () => {
                         <img className="mx-auto w-3/5 rounded-full ring-8 inline-block" src={bannar_image2} alt="" />
                     </div>
                     <div data-aos="fade-left" className="p-5 flex flex-col align-middle justify-items-center">
-                        <p className="font-black mb-5 text-justify">I'm a web developer with hands-on experience in JavaScript. I am passionate to build any kind of web development solution. I am an aspiring web developer, learning every aspect of web development. Alongside, I have achieved my CCNA certification. I have the experience to work as Technical Support Engineer. I am a quick learner, a team performer, and can stick to something until I achieve positive results.</p>
+                        <p className="font-black mb-5 text-justify">I'm a React Developer with hands-on experience. I am passionate to build any kind of web development solution. I am an aspiring web developer, learning every aspect of web development. I am a quick learner, a team performer, and can stick to something until I achieve positive results.</p>
                         <div data-aos="zoom-in" id="skills">
                             <h1 className="text-3xl font-bold border-l-4 p-1 border-blue-500">Skills</h1>
 
@@ -23,11 +23,11 @@ const AboutMe = () => {
                                 <h1 className="text-xl font-bold">Comfortable</h1>
                                 <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">JavaScript/ES6</div>
                                 <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">React.js</div>
-                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">MongoDB</div>
-                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">Node.js/Express.js</div>
+                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">Material UI</div>
                                 <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">REST API</div>
                                 <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">JSON</div>
                                 <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">DOM</div>
+                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">SASS</div>
                                 <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">Bootstrap</div>
                                 <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg">Tailwind</div>
                                 <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">HTML/CSS</div>
@@ -35,10 +35,11 @@ const AboutMe = () => {
 
                             <div id="familiar-skills" className="mt-3">
                                 <h1 className="text-xl font-bold">Familiar</h1>
-                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">React Native</div>
-                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">SASS</div>
-                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">Material UI</div>
-                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">OOP</div>
+                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">MongoDB</div>
+                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">Node.js/Express.js</div>
+                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">Redux</div>
+                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">PHP</div>
+                                <div className="bg-blue-300 font-black inline-block mr-3 mt-3 py-2 px-2 ring-2 ring-indigo-800 rounded-lg ">WordPress</div>
                             </div>
                             <div id="tools" className="mt-3">
                                 <h1 className="text-xl font-bold">Tools</h1>
