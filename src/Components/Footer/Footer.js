97d48cbd2dbb8d715f5211c6d75d54ca8d013cf8
Md Tawsif Ul Hye Chowdhury <tawsif.lucse@gmail.com>
2022-07-22 10:37:03 +0600

    footer modified

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <>
             <div className="footer p-16">
-                <p className="font-black text-center">COPYRIGHT &copy;2021 TAWSIF ALL RIGHTS RESERVED</p>
+                <p className="font-black text-center">COPYRIGHT &copy;2022 TAWSIF ALL RIGHTS RESERVED</p>
             </div>
         </>
     );
